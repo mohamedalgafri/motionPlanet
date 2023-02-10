7c3b03247905b0937bc562262519a92eece3cc4a
mohamedalgafri <mohamed18540m@gmail.com>
2023-02-10 08:30:12 -0800

    up2

--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -21,7 +21,7 @@ const Feedback = () => (
         <div className="feedback-gradient" />
         <div>
           <h4 className="font-bold sm:text-[32px] text-[26px] sm:leading-[40.32px] leading-[36.32px] text-white">
-          Muhyi al-Jaffari
+          Mohy Alghafari
           </h4>
           <p className="mt-[8px] font-normal sm:text-[18px] text-[12px] sm:leading-[22.68px] leading-[16.68px] text-white">
           Founder of MotionBanette
